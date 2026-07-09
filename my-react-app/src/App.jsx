6fc52138a8ca5df2970c076b88eb657bf24e8d2f
adityasikawarr <adityasikarwarr156@gmail.com>
2026-07-09 11:56:06 +0530

    made a card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,8 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
+import Card from "./Card.jsx";
 function App() {
   return (
     <div>
-      {" "}
-      <Header></Header>
-      <Footer></Footer>
-      <Food></Food>
+      <Card />
     </div>
   );
 }
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card(){
+return(
+    <div className = "card">
+        {/* <img src = "" alt="Aditya Sikarwarr"></img> */}
+        <h2 className="card-title">Aditya Sikarwarr</h2>
+        <p className="card-content">I am a software engineer and a billionaire</p>
+    </div>
+)
+}export default Card
+
