edcc7395ead529c8137ecc85ef42f98a82436ff7
adityasikawarr <adityasikarwarr156@gmail.com>
2026-07-10 20:30:49 +0530

    conditional rendring

--- my-react-app/src/userGreeting.jsx
+++ my-react-app/src/userGreeting.jsx
@@ -2,6 +2,8 @@ function UserGreeting(props) {
    if (props.isLoggedIn){
        return <h1>Welcome {props.username}</h1>
    }
+    return <h1>Please log in to continue</h1>
+   
 
 }
 export default UserGreeting;
