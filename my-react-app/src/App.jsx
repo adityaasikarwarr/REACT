708a829e1d89626d376b73b369a05178cef04af9
adityasikawarr <adityasikarwarr156@gmail.com>
2026-07-13 22:56:02 +0530

    file changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import ColorPicker from "./ColorPicker";
+
 function App() {
   // const fruits = [
   //   { id: 1, name: "Apple", calories: 95 },
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-     <ColorPicker></ColorPicker>
+
     </>
   );
 }
