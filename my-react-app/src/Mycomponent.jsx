c2381e91e2d279c0ec126ae73c2599f355adfdd6
adityasikawarr <adityasikarwarr156@gmail.com>
2026-07-19 23:21:41 +0530

    changes

--- my-react-app/src/Mycomponent.jsx
+++ my-react-app/src/Mycomponent.jsx
@@ -72,66 +72,66 @@ import React, { useState } from "react";
 
 // export default MyComponent;
 
-function MyComponent() {
-  const [cars, setCars] = useState([]);
-  const [carYear, setCarYear] = useState(new Date().getFullYear());
-  const [carMake, setCarMake] = useState("");
-  const [carModel, setCarModel] = useState("");
+// function MyComponent() {
+//   const [cars, setCars] = useState([]);
+//   const [carYear, setCarYear] = useState(new Date().getFullYear());
+//   const [carMake, setCarMake] = useState("");
+//   const [carModel, setCarModel] = useState("");
 
-  function handleAddCar() {
-    const newCar = { year: carYear, make: carMake, model: carModel };
-    setCars((c) => [...c, newCar]);
-    setCarYear(new Date().getFullYear());
-    setCarMake("");
-    setCarModel("");
-  }
-  function handleRemoveCar(index) {
-    setCars((c) => c.filter((_, i) => i !== index));
-  }
-  function handleYearChange(event) {
-    setCarYear(event.target.value);
-  }
-  function handleMakeChange(event) {
-    setCarMake(event.target.value);
-  }
-  function handleModelChange(event) {
-    setCarModel(event.target.value);
-  }
+//   function handleAddCar() {
+//     const newCar = { year: carYear, make: carMake, model: carModel };
+//     setCars((c) => [...c, newCar]);
+//     setCarYear(new Date().getFullYear());
+//     setCarMake("");
+//     setCarModel("");
+//   }
+//   function handleRemoveCar(index) {
+//     setCars((c) => c.filter((_, i) => i !== index));
+//   }
+//   function handleYearChange(event) {
+//     setCarYear(event.target.value);
+//   }
+//   function handleMakeChange(event) {
+//     setCarMake(event.target.value);
+//   }
+//   function handleModelChange(event) {
+//     setCarModel(event.target.value);
+//   }
 
-  return (
-    <div>
-      <h2>List of Cars</h2>
-      <ul>
-        {cars.map((car, index) => (
-          <li key={index}>
-            {car.year} {car.make} {car.model}
-            <button onClick={() => handleRemoveCar(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+//   return (
+//     <div>
+//       <h2>List of Cars</h2>
+//       <ul>
+//         {cars.map((car, index) => (
+//           <li key={index}>
+//             {car.year} {car.make} {car.model}
+//             <button onClick={() => handleRemoveCar(index)}>Remove</button>
+//           </li>
+//         ))}
+//       </ul>
 
-      <input type="number" value={carYear} onChange={handleYearChange} />
-      <br />
-      <input
-        type="text"
-        value={carMake}
-        onChange={handleMakeChange}
-        placeholder="Enter car make"
-      />
+//       <input type="number" value={carYear} onChange={handleYearChange} />
+//       <br />
+//       <input
+//         type="text"
+//         value={carMake}
+//         onChange={handleMakeChange}
+//         placeholder="Enter car make"
+//       />
 
-      <br />
+//       <br />
 
-      <input
-        type="text"
-        value={carModel}
-        onChange={handleModelChange}
-        placeholder="Enter car model"
-      />
+//       <input
+//         type="text"
+//         value={carModel}
+//         onChange={handleModelChange}
+//         placeholder="Enter car model"
+//       />
 
-      <br />
+//       <br />
 
-      <button onClick={handleAddCar}>Add car</button>
-    </div>
-  );
-}
-export default MyComponent;
+//       <button onClick={handleAddCar}>Add car</button>
+//     </div>
+//   );
+// }
+// export default MyComponent;
